f60359d2dff0ac0828affecbec97d266f21ce639
talhaansari77 <talhaprince09@gmail.com>
2023-06-22 14:00:05 +0300

    adminFormFixed

--- src/views/Addadmin.js
+++ src/views/Addadmin.js
@@ -1,123 +1,202 @@
-
-import React from "react";
-import '../assets/css/argon-dashboard-react.min.css';
-import {
-    Card,
-    Container, Row,
-    CardHeader,
-    Input,
-} from "reactstrap";
-
+import React, { useState } from "react";
+import "../assets/css/argon-dashboard-react.min.css";
+import { Card, Container, Row, CardHeader, Input } from "reactstrap";
 
 import Header from "components/Headers/Header.js";
-
-
-function Addadmin() {
-
-    return (
-        <>
-            <Header />
-
-            <div className="mb-3 p-4 mb-6 admin">
-                <div><h1 className="admin">Create Admin</h1></div>
-            </div>
-
-            {/* Page content */}
-            <Container className="mt--7 mb-5 bg-gradient-info " fluid>
-                {/* Dark table */}
-                <Row className="mt-5">
-                    <div className="col">
-                        <Card className="bg-default shadow">
-                            <CardHeader className="bg-transparent" style={{ borderBottom: "2px solid #666CA3" }}>
-                                <div>
-                                    <h1 className="text-center d-flex align-item-center justify-content-center">Add Admin</h1>
-                                </div>
-                            </CardHeader>
-
-                            <div className="d-flex justify-content-around pt-5">
-                                <div className="inputborder">
-                                    <Input className="inputborder" type="text" placeholder="Organaization"></Input>
-                                </div>
-                                <div className="inputborder">
-                                    <Input className="inputborder" type="text" placeholder="Type Event"></Input>
-                                </div>
-                                <div>
-                                    {/* <Input>asdf</Input> */}
-                                </div>
-                            </div>
-                            <div className="pt-5 d-flex justify-content-center" >
-                                <div className="inputborder" style={{ width: "100%", paddingRight: "1%" }}>
-                                    <Input className="inputborder" type="text" placeholder="location"></Input>
-                                </div>
-                            </div>
-                            <div className="d-flex justify-content-around pt-5">
-                                <div className="inputborder">
-                                    <lable className="evnetcolor">Prep Event Start Time</lable>
-                                    <Input className="inputborder" type="date" placeholder="Enter a date"></Input>
-                                </div>
-                                <div className="inputborder">
-                                <lable className="evnetcolor">Prep Event End Time</lable>
-                                    <Input className="inputborder" type="date" placeholder="Type Event"></Input>
-                                </div>
-                                <div>
-                                    {/* <Input>asdf</Input> */}
-                                </div>
-                            </div>
-                            <div className="d-flex justify-content-around pt-5">
-                                <div className="inputborder">
-                                <lable className="evnetcolor">Event Start Time</lable>
-                                    <Input className="inputborder" type="date" placeholder="Enter a date"></Input>
-                                </div>
-                                <div className="inputborder">
-                                <lable className="evnetcolor">Event End Time</lable>
-                                    <Input className="inputborder" type="date" placeholder="Type Event"></Input>
-                                </div>
-                                <div>
-                                    {/* <Input>asdf</Input> */}
-                                </div>
-                            </div>
-
-                            <div className="d-flex justify-content-around pt-5">
-                                <div className="inputborder">
-                                <lable className="evnetcolor">Clean Up Start Time</lable>
-                                    <Input className="inputborder" type="date" placeholder="Enter a date"></Input>
-                                </div>
-                                <div className="inputborder">
-                                <lable className="evnetcolor">Clean Up End Time</lable>
-                                    <Input className="inputborder" type="date" placeholder="Type Event"></Input>
-                                </div>
-                                <div>
-                                    {/* <Input>asdf</Input> */}
-                                </div>
-                            </div>
-                            <div className="d-flex pb-4 justify-content-around pt-4">
-                                <div className="inputborder" style={{marginTop:"23px"}}>
-                                    
-                                    <Input className="inputborder" type="text" placeholder="Event Capacity"></Input>
-                                </div>
-                                <div className="inputborder">
-                                    <select class="form-select pt-3 inputborder" style={{colo:"#666CA3"}} aria-label="Default select example">
-                                        <option selected>Group service period</option>
-                                        <option value="1">One</option>
-                                        <option value="2">Two</option>
-                                        <option value="3">Three</option>
-                                    </select>
-                                </div>
-                                <div>
-                                    {/* <Input>asdf</Input> */}
-                                </div>
-                            </div>
-
-                            <div className="addevents">
-                                <button className="mainbuttons">Add Event</button>
-                            </div>
-
-                        </Card>
-                    </div>
-                </Row>
-            </Container>
-        </>
-    );
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Lottie from "react-lottie";
+import loaderAnimation from "assets/Loaders";
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: loaderAnimation,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
 };
+function Addadmin() {
+  const [loading, setLoading] = useState(false);
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [state, setState] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    phoneNumber: "",
+    address: "",
+    organization: "",
+  });
+
+  const navigate = useNavigate();
+
+  const navigateHome = () => {
+    // 👇️ navigate to /
+    navigate("/admin/index");
+  };
+  const onSubmit = async () => {
+    setLoading(true)
+   await axios
+      .post("https://cutz-production.up.railway.app/api/v1/admin/signup", state)
+      .then((r) => {
+        setLoading(false)
+        navigateHome();
+        // alert(r.data.message)
+      })
+      .catch((e) => {
+        setLoading(false)
+        alert(e);
+      });
+  };
+
+  return (
+    <>
+      <Header />
+
+      <div className="mb-3 p-4 mb-6 admin">
+        <div>
+          <h1 className="admin">Create Admin</h1>
+        </div>
+      </div>
+
+      {/* Page content */}
+      <Container className="mt--7 mb-5 bg-gradient-info " fluid>
+        {/* Dark table */}
+        <Row className="mt-5">
+          <div className="col">
+            <Card className="bg-default shadow">
+              <CardHeader
+                className="bg-transparent"
+                style={{ borderBottom: "2px solid #666CA3" }}
+              >
+                <div>
+                  <h1 className="text-center d-flex align-item-center justify-content-center">
+                    Add Admin
+                  </h1>
+                </div>
+              </CardHeader>
+
+              <div className="d-flex justify-content-around pt-5">
+                <div className="inputborder">
+                  <Input
+                    className="inputborder"
+                    type="text"
+                    placeholder="First Name"
+                    value={state.firstName}
+                    onChange={(e) =>
+                      setState({ ...state, firstName: e.target.value })
+                    }
+                  ></Input>
+                </div>
+                <div className="inputborder">
+                  <Input
+                    className="inputborder"
+                    type="text"
+                    placeholder="Last Name"
+                    value={state.lastName}
+                    onChange={(e) =>
+                      setState({ ...state, lastName: e.target.value })
+                    }
+                  ></Input>
+                </div>
+                <div>{/* <Input>asdf</Input> */}</div>
+              </div>
+              <div className="d-flex justify-content-around pt-5">
+                <div className="inputborder">
+                  <Input
+                    className="inputborder"
+                    type="email"
+                    placeholder="Email"
+                    value={state.email}
+                    onChange={(e) =>
+                      setState({ ...state, email: e.target.value })
+                    }
+                  ></Input>
+                </div>
+                <div className="inputborder">
+                  <Input
+                    className="inputborder"
+                    type="tel"
+                    placeholder="Phone"
+                    value={state.phoneNumber}
+                    onChange={(e) =>
+                      setState({ ...state, phoneNumber: e.target.value })
+                    }
+                  ></Input>
+                </div>
+                <div>{/* <Input>asdf</Input> */}</div>
+              </div>
+              <div className="d-flex justify-content-around pt-5">
+                <div className="inputborder">
+                  <Input
+                    className="inputborder"
+                    type="password"
+                    placeholder="Password"
+                    value={state.password}
+                    onChange={(e) =>
+                      setState({ ...state, password: e.target.value })
+                    }
+                  ></Input>
+                </div>
+                <div className="inputborder">
+                  <Input
+                    className="inputborder"
+                    type="password"
+                    placeholder="Confirm Password"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                  ></Input>
+                </div>
+                <div>{/* <Input>asdf</Input> */}</div>
+              </div>
+              <div className="d-flex justify-content-around pt-5">
+                <div className="inputborder">
+                  <Input
+                    className="inputborder"
+                    type="text"
+                    placeholder="Organaization"
+                    value={state.organization}
+                    onChange={(e) =>
+                      setState({ ...state, organization: e.target.value })
+                    }
+                  ></Input>
+                </div>
+                <div className="inputborder">
+                  <Input
+                    className="inputborder"
+                    type="text"
+                    placeholder="Address"
+                    value={state.address}
+                    onChange={(e) =>
+                      setState({ ...state, address: e.target.value })
+                    }
+                  ></Input>
+                </div>
+                <div>{/* <Input>asdf</Input> */}</div>
+              </div>
+
+              <div className="addevents">
+                <button className="mainbuttons" onClick={onSubmit}>
+                  Add Event
+                </button>
+                {loading ? (
+              <Lottie
+                style={{}}
+                options={defaultOptions}
+                height={30}
+                width={30}
+                isClickToPauseDisabled
+              />
+            ) : (
+              <></>
+            )}
+              </div>
+            </Card>
+          </div>
+        </Row>
+      </Container>
+    </>
+  );
+}
 
 export default Addadmin;
